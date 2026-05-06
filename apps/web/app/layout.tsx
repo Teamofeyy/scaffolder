6cc9f0895bfd4b444cd7cd360c3a4557a6afe5c6
Teamofeyy <timarnd06@gmail.com>
2026-05-06 23:23:54 +0300

    - vercel

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,16 +5,16 @@ import { Suspense } from "react";
 import { ThemeProvider } from "./ui/theme-provider";
 import { Toaster } from '@/components/ui/sonner'
 
-export const metadataBase = new URL("https://scaffolder-sigma.vercel.app/");
+export const metadataBase = new URL("https://scaffolder.teamofey.tech/");
 
 export const metadata: Metadata = {
 
   title: "Frontend Scaffolder - Создайте проект за секунды",
   description: "Современный инструмент для генерации структуры frontend проектов",
-  metadataBase: new URL("https://scaffolder-sigma.vercel.app/"),
+  metadataBase: new URL("https://scaffolder.teamofey.tech/"),
   openGraph: {
     siteName: "Frontend Scaffolder",
-    url: "https://scaffolder-sigma.vercel.app/",
+    url: "https://scaffolder.teamofey.tech/",
     images: [
       {
         url: "/main-image.png",
